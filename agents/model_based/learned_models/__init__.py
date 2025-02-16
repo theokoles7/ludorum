8ca0215f52549c79e5a19f01deb533201ed9dcef
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2025-02-16 14:22:41 -0600

    Update model-based agents package

---
+++ agents/model_based/learned_models/__init__.py
@@ -0,0 +1,6 @@
+"""Model-based Learned Models agents package."""
+
+__all__ = ["DreamerAgent", "WorldAgent"]
+
+from agents.model_based.learned_models.dreamer      import DreamerAgent
+from agents.model_based.learned_models.world_model  import WorldAgent
---
+++ agents/model_based/learned_models/dreamer.py
@@ -0,0 +1,15 @@
+"""Implementation of Dreamer agent as proposed in the 2019 paper "Dream to Control: Learning 
+Behaviors by Latent Imagination" by Hafner et al."""
+
+__all__ = ["DreamerAgent"]
+
+class DreamerAgent():
+    """Dreamer Agent class based on the original 2019 paper, by Hafner et al.
+    
+    Link to paper: https://arxiv.org/pdf/1912.01603
+    """
+    
+    def __init__(self):
+        """Initialize Dreamer agent class."""
+        
+        raise NotImplementedError(f"Dreamer agent has not been implemented.")
