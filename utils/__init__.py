cd2896e0dfd00e8b372f544d0b5177afc26f69e2
theokoles7 <gabrieltrahan777@hotmail.com>
2025-02-17 08:20:19 -0600

    Add global timestamp

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ["arguments", "banner", "logger"]
+__all__ = ["ARGS", "BANNER", "LOGGER", "TIMESTAMP"]
 
 from utils.arguments    import ARGS
 from utils.banner       import BANNER
 from utils.logger       import LOGGER
+from utils.timestamp    import TIMESTAMP
