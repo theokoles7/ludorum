f885eba63354368d4f3ffc88d7e5429677a09ff1
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2025-05-09 13:34:24 -0500

    Update dependencies

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     install_requires =  [
                             "numpy",
                             "setuptools",
+                            "termcolor",
                             "torch",
                             "tqdm"
                         ]
