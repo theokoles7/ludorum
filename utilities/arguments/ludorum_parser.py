c09486c1e825f2f7b59b4407111c8ed65a5d1f0f
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2025-05-09 10:58:22 -0500

    Initialize argument parsing

---
+++ utilities/arguments/ludorum_parser.py
@@ -0,0 +1,49 @@
+"""Ludorum application argument definitions and parsing."""
+
+from argparse   import ArgumentParser, _ArgumentGroup, Namespace, _SubParsersAction
+
+# Initialize primary parser
+_parser_:       ArgumentParser =    ArgumentParser(
+    prog =          "ludorum",
+    description =   """Suite of environments, models, & methods in pursuit of achieving organic 
+                    reasoning & logic by means of deep reinforcement learning."""
+)
+
+# Initialize sub-parser
+_subparser_:    _SubParsersAction = _parser_.add_subparsers(
+    dest =          "command",
+    help =          "Ludorum commands."
+)
+
+# +================================================================================================+
+# | BEGIN ARGUMENTS                                                                                |
+# +================================================================================================+
+
+# LOGGING ==========================================================================================
+_logging:       _ArgumentGroup =    _parser_.add_argument_group(
+    title =         "Logging",
+    description =   "Logging configuration."    
+)
+
+_logging.add_argument(
+    "--logging-level",
+    type =          str,
+    choices =       ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+    default =       "INFO",
+    help =          """Minimum logging level (DEBUG < INFO < WARNING < ERROR < CRITICAL). Defaults 
+                    to "INFO"."""
+)
+
+_logging.add_argument(
+    "--logging-path",
+    type =          str,
+    default =       "logs",
+    help =          """Path at which logs will be written. Defaults to "./logs/"."""
+)
+
+# +================================================================================================+
+# | END ARGUMENTS                                                                                  |
+# +================================================================================================+
+
+# Parse arguments
+ARGS:       Namespace =         _parser_.parse_args()
